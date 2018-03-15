5bb465b411d6e197624dda002b88e0392c8d7511
Alfi Rizka <avikaco@gmail.com>
2018-03-16 00:18:52 +0700

    remove comment

--- src/V1/Auth/HasherProvider.php
+++ src/V1/Auth/HasherProvider.php
@@ -3,13 +3,6 @@ namespace Ax\Neo\V1\Auth;
 
 use Illuminate\Support\ServiceProvider;
 
-/*
- * TODO
- * 1. jangan lupa di config app.php bagian providers dikomen
- * // Illuminate\Hashing\HashServiceProvider::class,
- * 2. ganti jadi
- * Ax\Neo\V1\Auth\HasherProvider::class,
- */
 class HasherProvider extends ServiceProvider
 {
 
@@ -20,7 +13,6 @@ class HasherProvider extends ServiceProvider
      */
     public function register()
     {
-        // pakai ini kalo laravel 5.4+
         $this->app->singleton('hash', function () {
             return new Hasher();
         });
