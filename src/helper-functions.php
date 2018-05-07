0a3d58e7e4d3f1d9fd52550690cc57f203f4a875
Alfi Rizka <avikaco@gmail.com>
2018-05-08 03:46:59 +0700

    fix cut of time format

--- src/helper-functions.php
+++ src/helper-functions.php
@@ -2,7 +2,7 @@
 
 function ax_date_today()
 {
-    $cutOffTime = env('NEO_CUT_OFF_TIME', 8);
+    $cutOffTime = env('NEO_CUT_OFF_TIME', '08:00:00');
     if (date('H:i:s') < $cutOffTime) {
         return date('Y-m-d', strtotime('yesterday'));
     } else {
